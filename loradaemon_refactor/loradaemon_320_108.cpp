187228a1ac20ed11bb1ba4ba8d45654f46c96d90
makrohard <410733@gmail.com>
2026-05-17 04:35:32 +0200

    daemon include cleanup

--- loradaemon_refactor/loradaemon_320_108.cpp
+++ loradaemon_refactor/loradaemon_320_108.cpp
@@ -131,9 +131,7 @@
 #include <stdint.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <sys/select.h>
 #include <arpa/inet.h>
-//#include <fcntl.h>
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/stat.h>
